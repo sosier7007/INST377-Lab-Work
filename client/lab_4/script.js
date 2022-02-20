c594b89652aa66abd0e220edb45d2e43147257e3
sosier7007 <sosier@terpmail.umd.edu>
2022-02-19 22:40:30 -0500

    working carousel for lab 4

---
+++ client/lab_4/script.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-restricted-syntax */
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel_item');
+const totalSlides = slides.length;
+
+function updateSlidePosition() {
+  for (const slide of slides) {
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
+
+  updateSlidePosition();
+}
+
+document
+  .querySelector('#carousel_button--next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
+
+document
+  .querySelector('#carousel_button--prev')
+  .addEventListener('click', () => {
+    moveToPrevSlide();
+  });
