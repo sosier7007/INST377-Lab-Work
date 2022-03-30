c3ab7f2320c75e51bc4036b15e636deb289b163a
sosier7007 <sosier@terpmail.umd.edu>
2022-03-30 15:15:18 -0400

    lab 6 finished

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -4,12 +4,33 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // inclusive
 }
 
-function dataHandler(dataArray) {
-  console.log('fired dataHandler');
-  console.table(dataArray); // this is called "dot notation"
+function restoArrayMake(dataArray) {
+  // console.log('fired dataHandler');
+  // console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('range item', item);
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+
+  console.log(listItems);
+  return listItems;
+
+  // range.forEach((item) => {
+  // console.log('range item', item);
+  // });
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
   });
 }
 
@@ -31,7 +52,8 @@ async function mainEvent() { // the async keyword means we can make API requests
       console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
