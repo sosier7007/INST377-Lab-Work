e53a8f0cb7d4d2a67884250ab90b07bae1e1992c
sosier7007 <sosier@terpmail.umd.edu>
2022-03-30 12:55:35 -0400

    fixed linting errors

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,6 +1,16 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // inclusive
+}
+
 function dataHandler(dataArray) {
-  console.log('fired dataHandler')
+  console.log('fired dataHandler');
   console.table(dataArray); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
+  });
 }
 
 // hi
@@ -8,12 +18,15 @@ async function mainEvent() { // the async keyword means we can make API requests
   console.log('script loaded');
   const form = document.querySelector('.main_form');
   const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none';
+
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   if (arrayFromJson.data.length > 0) {
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
+      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       dataHandler(arrayFromJson.data);
