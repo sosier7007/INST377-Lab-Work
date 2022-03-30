2c9ff4f8b9f5ff912fed31933a91f5ba055ee6c0
sosier7007 <sosier@terpmail.umd.edu>
2022-03-30 13:18:01 -0400

    submit button appears

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -22,11 +22,13 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+
   if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       dataHandler(arrayFromJson.data);
